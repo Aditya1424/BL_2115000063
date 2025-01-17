decfbe268e9406eb52f4fe7d9983597f600558b8
Aditya Gupta <ag7023587@gmail.com>
2025-01-17 14:23:37 +0530

    smallest code added

---
+++ src/smallest.java
@@ -0,0 +1,13 @@
+public class smallest {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+        if(a<b){
+            System.out.println("Smallest");
+        }
+        else{
+            System.out.println("Largest");
+        }
+    }
+}
